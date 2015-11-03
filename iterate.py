de165c1ba24cd76beac06f237e24622b2244c0ab
Eric Angle <eric.lohner.angle@gmail.com>
2015-11-03 06:04:12 -0700

    tests run twice as fast after using numpy.allclose for comparison

--- iterate.py
+++ iterate.py
@@ -1,26 +1,15 @@
-from numpy import *
+import numpy as np
 import math
-
-def keepGoing(old,new,tol):
-  tol = tol/1.0e3
-  stillGTtol = 0
-  for i in ndindex(old.shape):
-    if abs(old[i] - new[i]) > tol:
-      stillGTtol = stillGTtol + 1
-  if stillGTtol == 0:
-    return 0
-  else:
-    return 1
 
 # Solves A x = B iteratively
 # This function is equivalent to the Jacobi relaxation method.
 def iterative(A,B,tol):
-  x = zeros(((B.shape)[0]))
-  oldX = copy(x)
+  x = np.zeros(((B.shape)[0]))
+  oldX = np.copy(x)
   count = 0
-  while keepGoing(oldX,x,tol) == 1 or count == 0:
+  while np.allclose(oldX,x,tol/10.0,0.0) == False or count == 0:
     count = count + 1
-    oldX = copy(x)
-    for i in ndindex(x.shape):
-      x[i] = oldX[i] + (1.0/A[i][i])*(B[i] - dot(A,oldX)[i])
+    oldX = np.copy(x)
+    for i in np.ndindex(x.shape):
+      x[i] = oldX[i] + (1.0/A[i][i])*(B[i] - np.dot(A,oldX)[i])
   return x
