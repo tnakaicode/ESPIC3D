403fe71b097dba60ec47677cbb5070f0bfb98577
Eric Angle <eric.lohner.angle@gmail.com>
2015-11-03 06:08:35 -0700

    removing unneccesary count variable

--- iterate.py
+++ iterate.py
@@ -5,10 +5,8 @@ import math
 # This function is equivalent to the Jacobi relaxation method.
 def iterative(A,B,tol):
   x = np.zeros(((B.shape)[0]))
-  oldX = np.copy(x)
-  count = 0
-  while np.allclose(oldX,x,tol/10.0,0.0) == False or count == 0:
-    count = count + 1
+  oldX = np.ones(((B.shape)[0]))
+  while np.allclose(oldX,x,tol/10.0,0.0) == False:
     oldX = np.copy(x)
     for i in np.ndindex(x.shape):
       x[i] = oldX[i] + (1.0/A[i][i])*(B[i] - np.dot(A,oldX)[i])
