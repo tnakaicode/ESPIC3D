6544cddafc40983e8de42f1832daa92fa5dab931
Eric Angle <eric.lohner.angle@gmail.com>
2015-11-03 19:48:59 -0700

    adding max number of iterations

--- iterate.py
+++ iterate.py
@@ -1,13 +1,18 @@
 import numpy as np
 import math
 
+maxIterations = 10000
+
 # Solves A x = B iteratively
 # This function is equivalent to the Jacobi relaxation method.
 def iterative(A,B,tol):
   x = np.zeros(((B.shape)[0]))
   oldX = np.ones(((B.shape)[0]))
-  while np.allclose(oldX,x,tol/10.0,0.0) == False:
+  for j in xrange(maxIterations):
     oldX = np.copy(x)
     for i in np.ndindex(x.shape):
-      x[i] = oldX[i] + (1.0/A[i][i])*(B[i] - np.dot(A,oldX)[i])
+      if A[i][i] != 0.0:
+        x[i] = oldX[i] + (1.0/A[i][i])*(B[i] - np.dot(A,oldX)[i])
+    if np.allclose(oldX,x,tol/10.0,0.0) == True:
+      break
   return x
