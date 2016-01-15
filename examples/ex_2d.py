f5d97aa75f2f29c8fe217e89340fd6304906d8dd
Eric Angle <eric.lohner.angle@gmail.com>
2016-01-14 20:48:10 -0700

    removing unnecessary imports

--- examples/ex_2d.py
+++ examples/ex_2d.py
@@ -1,14 +1,10 @@
 import os
 import sys
-import matplotlib as ml
 import matplotlib.pyplot as plt
 import numpy as np
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/..')
 import esSolve
 from mpl_toolkits.mplot3d import axes3d
-import math
-
-plt.show()
 
 NX = 22
 NY = 23
